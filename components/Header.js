48f41c5b788ff405be2f8d1e7fc4b5afbccf3e2d
Johnny <15818464582@163.com>
2020-04-15 23:41:21 +0800

    first commit
    Change-Id: If3fc50998ccd64cd37163aab51851a5b43e37bdf

---
+++ components/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './header.css'
+import {Row, Col, Menu, Icon} from 'antd'
+
+const Header =  () => (
+	<div className="header">
+	<Row type="flex" justify="center">
+		<Col>	
+			<span className="">标题 </span>
+		</Col>
+		<Col  className="memu-div" xs={0} sm={0} md={14} lg={8} xl={6}>	
+			<Menu mode="horizontal">
+				<Menu.Item key="home">
+					<Icon type="home" />
+					首页
+				</Menu.Item>
+				<Menu.Item key="video">
+					<Icon type="youtube" />
+					视频
+				</Menu.Item>
+				<Menu.Item key="live">
+					<Icon type="smile" />
+					生活
+				</Menu.Item>
+			</Menu>
+
+		</Col>
+
+	</Row>
+	</div>
+)
+
+export default Header;
